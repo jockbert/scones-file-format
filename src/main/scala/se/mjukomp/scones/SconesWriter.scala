25a76671aca9f1e3dce0f7d0b836719e42443e92
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2018-06-16 10:41:47 +0200

    Add capability to write groups

--- src/main/scala/se/mjukomp/scones/SconesWriter.scala
+++ src/main/scala/se/mjukomp/scones/SconesWriter.scala
@@ -2,10 +2,15 @@ package se.mjukomp.scones
 
 case class SconesWriter() {
 
-  def write(scones: List[Scone]): String =
+  def write(
+    scones: List[Scone],
+    indent: Int         = 0): String = {
+
+    val padding = "    " * indent
+
     scones.map({
       case Leaf(text) => text
-      case _          => Stream.empty[Char]
-    }).mkString("", "\n", "\n")
-
+      case Group(sub) => "(" + write(sub, indent + 1).trim() + ")"
+    }).mkString(padding, "\n" + padding, "\n")
+  }
 }
--- src/test/scala/se/mjukomp/scones/WriterProps.scala
+++ src/test/scala/se/mjukomp/scones/WriterProps.scala
@@ -3,10 +3,14 @@ package se.mjukomp.scones
 import org.scalacheck.Properties
 import org.scalacheck.Prop
 import org.scalacheck.Prop._
+import se.mjukomp.scones.Scone._
 
 object WriterProps extends Properties("SconesWriter") {
 
   val writer = SconesWriter()
+
+  def write(scones: Scone*): String =
+    writer.write(scones.toList)
 
   property("EmptyData") =
     write() ?= "\n"
@@ -20,6 +24,15 @@ object WriterProps extends Properties("SconesWriter") {
          |Two
          |""".stripMargin
 
-  def write(scones: Scone*) =
-    writer.write(scones.toList)
+  property("OneGroup") =
+    write(group("One", "Two", "Three")) ?=
+      """|(One
+         |    Two
+         |    Three)
+         |""".stripMargin
+
+  property("EmptyGroup") =
+    write(group()) ?=
+      """|()
+         |""".stripMargin
 }
